575c5ee5adc13d33f2aa9e26e5450f615e783893
Kunalkhuteta <kunalkhuteta@gmail.com>
2026-02-18 15:38:32 +0530

    update for render -> cookies samesite for cross origin

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -1,4 +1,6 @@
 export const cookieOptions = {
     httpOnly: true,
     secure: true,
+    sameSite: "none" as const,  // required for cross-origin (Vercel → Render)
+  maxAge: 7 * 24 * 60 * 60 * 1000,
 }
